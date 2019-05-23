686859e7b22713910f949bb611e1418e562d1551
Nathan Brei <nbrei@jlab.org>
2019-05-23 15:26:13 -0400

    Now collecting CPU info

--- 2019-05-22/exp1/jtest.sh
+++ 2019-05-22/exp1/jtest.sh
@@ -9,4 +9,6 @@
 export JANA_HOME="/global/homes/n/nbrei/JANA2/Linux-x86_64-cc18.0.1"
 export RESULTS_DIR="/global/homes/n/nbrei/jana-perf-testing/2019-05-22/exp1"
 
+lscpu > $RESULTS_DIR/cpuinfo.txt
+
 srun $JANA_HOME/bin/jana -l $RESULTS_DIR/jana.config -Pbenchmark:resultsdir=$RESULTS_DIR -b > $RESULTS_DIR/stdout.txt
--- 2019-05-22/vtune/jtest.sh
+++ 2019-05-22/vtune/jtest.sh
@@ -11,6 +11,9 @@ export JANA_HOME="/global/homes/n/nbrei/JANA2/Linux-x86_64-cc18.0.1"
 export RESULTS_DIR="/global/homes/n/nbrei/jana-perf-testing/2019-05-22/vtune"
 
 module load vtune
+
+lscpu > $RESULTS_DIR/cpuinfo.txt
+
 srun amplxe-cl -collect general-exploration -r $RESULTS_DIR/vtune_results -- $JANA_HOME/bin/jana -l jana.config -Pbenchmark:resultsdir=$RESULTS_DIR > $RESULTS_DIR/stdout.txt
 
 
