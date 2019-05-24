afdddb634d621a36d064b15f400f6c16d6d8349c
Nathan Brei <nbrei@jlab.org>
2019-05-24 14:59:15 -0400

    Fixes to old_jtest_scaling Augur harness Now capturing _all_ stdout, which gives us more error messages

--- old_jtest_scaling/exp4/jtest_augur.sh
+++ old_jtest_scaling/exp4/jtest_augur.sh
@@ -5,4 +5,4 @@ export JANA_HOME="/home/nbrei/src/jana2/Linux_CentOS7-x86_64-gcc4.8.5"
 lscpu > cpuinfo.txt
 
 #$JANA_HOME/bin/jana -l jana.config -b > stdout.txt 2> stderr.txt
-$JANA_HOME/bin/jana -Pjana:legacy_mode=0 -Pbenchmark:minthreads=4 -Pbenchmark:maxthreads=88 -Pbenchmark:threadstep=4 -b > stdout.txt 2> stderr.txt
+$JANA_HOME/bin/jana -Pplugins=JTest -Pjana:legacy_mode=0 -Pnthreads=4 -Pbenchmark:minthreads=4 -Pbenchmark:maxthreads=88 -Pbenchmark:threadstep=4 -b
