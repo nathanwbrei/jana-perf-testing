f75a7339e741ac2d2894f83fe46633dbdc3194e3
Nathan Brei <nbrei@jlab.org>
2019-07-15 15:47:41 -0400

    Preparing to run 'tuning' on coriknl

--- tuning/launch_cori.sh
+++ tuning/launch_cori.sh
@@ -7,8 +7,68 @@ set -x  # Echo on
 module swap craype-haswell craype-mic-knl
 module unload darshan
 
-cd /home/nbrei/src/jana2
+cd /global/homes/n/nbrei/JANA2
 git checkout be8fc5d94a6f7bcb4159101b05dc0523cd3e037d
 scons --clean
 scons -j8
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp1
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp2
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp3
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp4
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp5
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp6
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp7
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp8
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp9
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp10
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp11
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp12
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp13
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp14
+sbatch slurm.sh
+
+
+cd /global/homes/n/nbrei/jana-perf-testing/tuning/exp15
+sbatch slurm.sh
 
--- tuning/launch_farm.sh
+++ tuning/launch_farm.sh
@@ -7,53 +7,8 @@
 set -e  # Terminate script on error
 set -x  # Echo on
 
-cd /home/nbrei/src/jana2
+cd /global/homes/n/nbrei/JANA2
 git checkout be8fc5d94a6f7bcb4159101b05dc0523cd3e037d
 scons --clean
 scons -j8
 
-
-cd /home/nbrei/jana-perf-testing/tuning/exp1
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp2
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp3
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp4
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp5
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp6
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp7
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp8
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp9
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp10
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp11
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp12
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp13
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp14
-jsub -xml augur.xml
-
-cd /home/nbrei/jana-perf-testing/tuning/exp15
-jsub -xml augur.xml
